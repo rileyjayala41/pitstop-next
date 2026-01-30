d9bc523cd10d5e1f8fe26afae352391d8bcbb2f9
rileyjayala41 <rileyjayala41@gmail.com>
2026-01-29 23:41:37 -0600

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import "./globals.css";
 import SiteHeader from "@/components/SiteHeader";
@@ -39,6 +40,7 @@ export default function RootLayout({
         <SiteHeader />
         {children}
         <SiteFooter />
+	<Analytics />
       </body>
     </html>
   );
