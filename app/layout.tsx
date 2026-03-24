a806d8af283f613f32b68c44f8075bd4e4c5c8ce
rileyjayala41 <rileyjayala41@gmail.com>
2026-03-23 19:05:58 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,15 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-		  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18036747428"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-18036747428');
-</script>
         {/* Font (same as your HTML site) */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
